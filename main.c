a8756525aca251a3aa770e3c641674e03f9743e0
Soham Metha <sohammetha01@gmail.com>
2025-01-13 23:00:56 +0530

    removed examples

--- main.c
+++ main.c
@@ -2,37 +2,10 @@
 
 Vm vm = { 0 };
 
-char* srcCode = "PSH 0\n"
-                "PSH 1\n"
-                "DUP 2\n"
-                "DUP 2\n"
-                "ADD\n"
-                "JMP 2\n"
-                "HLT\n";
-/*
-Instruction ins[] = {
-    MAKE_INST_ARG(PSH, 0),
-    MAKE_INST_ARG(PSH, 1),
-    MAKE_INST_ARG(DUP, 2),
-    MAKE_INST_ARG(DUP, 2),
-    MAKE_INST(ADD),
-    MAKE_INST_ARG(JMP, 2),
-    MAKE_INST(HLT),
-};*/
-
 int main()
 {
-    // Program prog;
-    // memcpy(prog.instructions, ins, sizeof(ins));
-    // prog.instruction_count = sizeof(ins) / sizeof(ins[0]);
-    // prog.instruction_size = sizeof(ins[0]);
 
-    // assembleInstructionsIntoBinary(prog, "./prog/fibonacci.pbl");
-    // prog = loadBytecodeIntoProgram("./prog/fibonacci.pbl");
-    // loadProgram(&vm, prog);
-
-    String s = { strlen(srcCode), srcCode };
-    Program prog = translate_asm(s);
+    Program prog = translate_asm(loadFileIntoProgramString("prog/fibonacci.pblasm"));
     // loadProgram(&vm, prog);
 
     for (int i = 0; i < EXECUTION_LIMIT && !vm.cpu.halt; i++) {
